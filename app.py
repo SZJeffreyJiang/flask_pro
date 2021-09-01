ba3288f032a3257accfa671063051f635b728343
RongLe Jiang <rongle.jiang@compass.com>
2021-09-01 10:27:09 +0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+import configs
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!111'
+
+
+if __name__ == '__main__':
+    print(app.config)
+
+    app.run()
+
+
